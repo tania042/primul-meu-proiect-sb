f675f5e5047e3a2fb07413a9b71d9b92ea53f7e0
Tatiana Ceban <tatiana.ceban.19@bk.ru>
2025-05-14 20:24:29 +0300

    initial commit

---
+++ src/main/java/com/pixelacademy/springdemo_app/SpringdemoAppApplication.java
@@ -0,0 +1,13 @@
+package com.pixelacademy.springdemo_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdemoAppApplication.class, args);
+	}
+
+}
