b73c4543cd00744dafae024529fb3e87c9b2ef68
Tatiana Ceban <tatiana.ceban.19@bk.ru>
2025-05-15 17:49:27 +0300

    Add new endpoint

--- src/test/java/com/pixelacademy/springdemo_app/rest/HelloWorldRestController.java
+++ src/test/java/com/pixelacademy/springdemo_app/rest/HelloWorldRestController.java
@@ -1,4 +1,13 @@
 package com.pixelacademy.springdemo_app.rest;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloWorldRestController {
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello World!";
+    }
 }
